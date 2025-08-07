6b90733cc0ff5ebf59da50bd8d50c9bc6c866841
Piseth San <pisethsan67167@gmail.com>
2025-08-07 10:11:06 +0700

    docker

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+
+FROM node:23.7.0-alpine3.20
+WORKDIR /app
+COPY package.json package-lock.json ./
+RUN npm install
+COPY . .
+EXPOSE 3000
+CMD [ "npm", "run", "dev" ]
--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const dbName = "cadt-idg-003";
-const mongoURI = 'mongodb://localhost:27017';
+const mongoURI = 'mongodb://cadt-db:27017';
 
 export async function dbConnect() {
     mongoose.connection.on('connected', () => {
